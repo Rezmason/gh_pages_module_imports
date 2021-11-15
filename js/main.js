0bcd682cebdfd1f1ce89722606adb4fc7e37e56c
Rezmason <jeremysachs@rezmason.net>
2021-11-15 07:28:16 -0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import foo from "./foo.js";
+
+document.body.onload = async () => {
+	const dynamicImport = import("./solution/main.js");
+	const module = await dynamicImport;
+  console.log(foo, module.default));
+};
